e3dac3650d9f8335465f0b72f35c96293c797eac
Mac Pham <1777724+macpham@users.noreply.github.com>
2022-09-16 15:21:05 -0400

    Update Header.client.jsx

--- src/components/global/Header.client.jsx
+++ src/components/global/Header.client.jsx
@@ -141,7 +141,7 @@ function DesktopHeader({countryCode, isHome, menu, openCart, title}) {
     <header role="banner" className={styles.container}>
       <div className="flex gap-12">
         <Link className={`font-bold`} to="/">
-          🍁 Edition - {title}
+          Wee! - {title}
         </Link>
         <nav className="flex gap-8">
           {/* Top level menu items */}
