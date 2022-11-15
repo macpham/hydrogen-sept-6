ce684a811cf9d85781b64f2e9ae4b138f518407c
Mac Pham <1777724+macpham@users.noreply.github.com>
2022-11-15 15:51:22 -0500

    Bust stuff

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,11 +2,8 @@ import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    defaultCountryCode: 'CA',
-    defaultLanguageCode: 'EN',
     storeDomain: Oxygen.env.SHOPIFY_STORE_DOMAIN,
     storefrontToken: Oxygen.env.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN,
-    storefrontApiVersion: '2022-07',
   },
   session: CookieSessionStorage('__session', {
     path: '/',
